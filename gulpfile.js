41a77c389258050bfeb604bff04b8a93a216e666
Maksim_Selivanov <95679039+VizartLA@users.noreply.github.com>
2025-03-31 20:59:59 +0400

    Global fix gulp. package

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-const {src, dest, watch, parallel, series} = require('gulp');
-
-const scss   = require('gulp-sass')(require('sass'));
+const { src, dest, watch, parallel, series } = require('gulp');
+const sass = require('sass');
+const gulpSass = require('gulp-sass')(sass);
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default; 
+const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
@@ -12,118 +12,113 @@ const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
 const fonter = require('gulp-fonter');
 const ttf2woff2 = require('gulp-ttf2woff2');
-const svgSprite = require('gulp-svg-sprite');
 const include = require('gulp-include');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const svgSprite = require('gulp-svg-sprite'); // Добавлено
 
-function pages() {
-  return src('app/pages/*.html')
-    .pipe(include({
-      includePaths: 'app/components'
+// Убрана дублирующая функция styles
+function styles() {
+  return src('app/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(gulpSass({ 
+      outputStyle: 'compressed',
+      includePaths: ['app/scss'],
+      quietDeps: true
+    }).on('error', gulpSass.logError))
+    .pipe(autoprefixer({ 
+      overrideBrowserslist: ['last 4 versions'],
+      cascade: false
     }))
-    .pipe(dest('app'))
-    .pipe(browserSync.stream())
+    .pipe(concat('style.min.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream());
 }
 
-function fonts() {
-  return src('app/fonts/src/*.*')
-    .pipe(fonter({
-      formats: ['woff', 'ttf']
-    }))
-    .pipe(src('app/fonts/*.ttf'))
-    .pipe(ttf2woff2())
-    .pipe(dest('app/fonts'))
+
+function scripts() {
+  return src([
+    'node_modules/swiper/swiper-bundle.js',
+    'app/js/main.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
 }
 
-function images(){
+function images() {
   return src(['app/images/src/**/*.*', '!app/images/src/**/*.svg'])
     .pipe(newer('app/images'))
-    .pipe(avif({ quality : 50}))
-
+    .pipe(avif({ quality: 50 }))
+    .pipe(dest('app/images'))
     .pipe(src('app/images/src/**/*.*'))
     .pipe(newer('app/images'))
     .pipe(webp())
-
-    .pipe(src('app/images/src/**/*.*'))
-    .pipe(newer('app/images'))
-    .pipe(imagemin())
-
-    .pipe(dest('app/images'))
+    .pipe(dest('app/images'));
 }
 
-function sprite () {
+function sprite() {
   return src('app/images/**/*.svg')
     .pipe(svgSprite({
       mode: {
         stack: {
           sprite: '../sprite.svg',
-          example: true 
+          example: true
         }
       }
     }))
-    .pipe(dest('app/images'))
+    .pipe(dest('app/images'));
 }
 
-function scripts() {
-  return src([
-    "node_modules/swiper/swiper-bundle.js",
-    'app/js/main.js',
-  ])
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+function fonts() {
+  return src('app/fonts/src/*.*')
+    .pipe(fonter({ formats: ['woff', 'ttf'] }))
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'));
 }
 
-function styles() {
-  return src('app/scss/style.scss')
-    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version']}))
-    .pipe(concat('style.min.css'))
-    .pipe(scss({ outputStyle: 'compressed' }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+function pages() {
+  return src('app/pages/*.html')
+    .pipe(include({ includePaths: 'app/components' }))
+    .pipe(dest('app'))
+    .pipe(browserSync.stream());
 }
-
-function watching() {
-  browserSync.init({
-    server: {
-      baseDir: "app/"
-    }
-  });
-  watch(['app/scss/style.scss', 'app/scss/media.scss'], styles)
-  watch(['app/images/src'], images)
-  watch(['app/js/main.js'], scripts)
-  watch(['app/components/*', 'app/pages/*'], pages)
-  watch(['app/*.html']).on('change', browserSync.reload);
-}
-
 
 function cleanDist() {
-  return src('dist')
-    .pipe(clean())
+  return src('dist', { allowEmpty: true }).pipe(clean());
 }
 
 function building() {
   return src([
     'app/css/style.min.css',
-    '!app/images/**/*.html',
     'app/images/*.*',
     '!app/images/*.svg',
     'app/images/sprite.svg',
     'app/fonts/*.*',
     'app/js/main.min.js',
     'app/**/*.html'
-  ], {base : 'app'})
-    .pipe(dest('dist'))
+  ], { base: 'app' })
+    .pipe(dest('dist'));
+}
+
+function watching() {
+  browserSync.init({ server: { baseDir: "app/" } });
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/images/src'], images);
+  watch(['app/js/**/*.js'], scripts);
+  watch(['app/components/*', 'app/pages/*'], pages);
+  watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
+exports.scripts = scripts;
 exports.images = images;
 exports.fonts = fonts;
 exports.pages = pages;
-exports.building = building;
 exports.sprite = sprite;
-exports.scripts = scripts;
-exports.watching = watching;
-
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, images, scripts, pages, watching);
+exports.default = parallel(styles, scripts, images, fonts, pages, watching);
